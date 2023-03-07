b6b73af34236d3c84c8b2193a7ac9392aea5bde0
Israa Ezzat <israaezzat.133@gmail.com>
2023-03-07 14:21:40 +0200

    message container added

---
+++ src/Container/Messages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export  function Messages() {
+  return (
+    <div className='messages-container'>
+      
+    </div>
+  )
+}
---
+++ src/Container/NavigationBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function NavigationBar() {
+  return (
+    <div className='nav-container'>
+      
+    </div>
+  )
+}
